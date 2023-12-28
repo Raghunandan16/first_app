c7687b381a0c9fa3b7f27062fc35cf3cc172fa27
Raghunandan <16raghunandangupta@gmail.com>
2023-12-28 19:47:05 +0530

    fixed Test details

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,23 @@ import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: GradientContainer(
-        [
-          Colors.grey,
-          Color.fromARGB(255, 41, 1, 1),
-          Colors.red,
-        ],
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          [
+            Colors.grey,
+            Color.fromARGB(255, 41, 1, 1),
+            Colors.red,
+          ],
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
